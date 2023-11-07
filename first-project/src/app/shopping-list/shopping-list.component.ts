5285c2ba774659b476a1c94e256315c9ab38dab5
Raghuraj Thombare <raghurajthombare1997@gmail.com>
2023-11-07 21:42:52 +0530

    Added more data

--- first-project/src/app/shopping-list/shopping-list.component.ts
+++ first-project/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ingredients } from '../shared/Ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
+  ingredients: Ingredients[] = [
+    new Ingredients('Apples', 5),
+    new Ingredients('Tomatoes', 10)
+  ]
 
 }
